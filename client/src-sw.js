1dbcbe2300dae9d5275dc5f65148c124a01de51d
Artemis051 <aekatsi@gmail.com>
2024-03-13 19:09:07 -0400

    final commit

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,27 +28,15 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  // This function defines the types of requests to cache. 
-  // It caches requests for stylesheets, scripts, and web workers.
   ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
 
-  // This specifies the caching strategy to use, which is 'StaleWhileRevalidate' in this case.
-  // 'StaleWhileRevalidate' means it will use the cached response first, then update it with the latest one from the network for next use.
   new StaleWhileRevalidate({
-    // 'asset-cache' is the name of the cache where these assets will be stored.
     cacheName: 'asset-cache',
-
-    // The 'plugins' array allows adding features to the caching strategy.
     plugins: [
-      // CacheableResponsePlugin decides which responses are considered cacheable.
       new CacheableResponsePlugin({
-        // caches responses with status code 200 and 0 
         statuses: [0, 200],
       }),
-
-      // ExpirationPlugin controls how long the cached entries are stored
       new ExpirationPlugin({
-        // Cached entries will be kept for 30 days (30 days * 24 hours * 60 minutes * 60 seconds).
         maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
